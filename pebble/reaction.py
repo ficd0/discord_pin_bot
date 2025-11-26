f4bb7c6ad5cce9f02792a9fdc42474b6b3869abe
Daniel Fichtinger <daniel@ficd.sh>
2025-11-25 19:15:33 -0500

    added more emoji mappings

--- pebble/reaction.py
+++ pebble/reaction.py
@@ -3,7 +3,12 @@ import re
 from main import DEBUG
 
 # stores the mapping of pattern -> emoji reactions
-_REACTION_MAP: dict[str, str] = {r"smok": "<:ramoke:1310422073562365972>"}
+_REACTION_MAP: dict[str, str] = {
+    r"smok": "<:ramoke:1310422073562365972>",
+    r"\bgnu(\+|/)linux\b": "🐧",
+    r"\bblazing(ly)?.fast\b": "🦀",
+    r"\bhom(o|ie)sexual\b": "🏳️‍🌈",
+}
 if DEBUG:
     _REACTION_MAP[r"unicode"] = "👍"
     _REACTION_MAP[r"skibidi"] = "<:SKIBIDI:1425551738370396301>"
@@ -14,6 +19,6 @@ if DEBUG:
 def reacts_with(s: str) -> list[str]:
     out: list[str] = []
     for k in _REACTION_MAP:
-        if re.search(k, s):
+        if re.search(k, s.lower()):
             out.append(_REACTION_MAP[k])
     return out
